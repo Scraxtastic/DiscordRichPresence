7413de09ac5c339220b861c2684063e320b6af2d
Scraxtastic <nick.u19990@gmail.com>
2021-06-13 16:21:36 +0200

    Rollback, because of unpredictable error messages

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var rpc = require("discord-rpc")
-// the client is needed in multiple functions
-let client;
+let client = new rpc.Client({ transport: 'ipc' })
+let clientConnected = false;
 
 /**
  * Logging in file is not yet implemented. It is used as a placeholder
@@ -32,7 +32,9 @@ const refreshTime = 10;
 /**
  * this needs to be filled. This is used as a fallback, if no appId is given in the statusConentListItem
  */
-const defaultAppId = "852680799799214080";
+const defaultAppId = "852697412421287946";
+
+const defaultImage = "scraxicon";
 
 /**
  * TODO Forcechange if the random number is equal to the current index
@@ -54,8 +56,9 @@ const forceChange = false;
  */
 const statusContentList = [
     { details: "Not giving up", text: "Never gonna let you down", image: "scraxicon", buttons: [{ label: "Important Video", url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ" }] },
-    { details: "Baiting people", text: "Baiting you!", image: "scraxicon", appId: "853439830146416650", buttons: [{ label: "Bait", url: "https://youtu.be/d1YBv2mWll0" }] },
-    { details: "Playing games", text: "Actually not", image: "scraxicon", appId: "852697412421287946" }
+    { details: "Baiting people", text: "Baiting you!", image: "scraxicon", buttons: [{ label: "Bait", url: "https://youtu.be/d1YBv2mWll0" }] },
+    { details: "Working hard", text: "Actually not", image: "scraxicon" },
+    { details: "Klicking zirkles", text: "Hmm, what am i doing here?" }
 ]
 
 
@@ -96,12 +99,12 @@ const log = (logMessage) => {
  * @param {details: string, text: string, buttons: [{label: string, url: string}], image: string} param0 
  * @returns the activity
  */
-const getActivity = ({ details, text, buttons, imageName }) => {
+const getActivity = ({ details, text, buttons, image }) => {
     if (buttons) {
         return {
             details: details || "no details given...",
             assets: {
-                large_image: imageName, //Image name that is set in the application
+                large_image: image || defaultImage, //Image name that is set in the application
                 large_text: text || "No text given..." // this will show as "Playing <Status>" from the outisde
             },
             buttons: buttons,
@@ -110,7 +113,7 @@ const getActivity = ({ details, text, buttons, imageName }) => {
     return {
         details: details || "no details given...",
         assets: {
-            large_image: imageName, //Image name that is set in the application
+            large_image: image || defaultImage, //Image name that is set in the application
             large_text: text || "No text given..." // this will show as "Playing <Status>" from the outisde
         }
     }
@@ -133,13 +136,16 @@ const updateStatus = () => {
  * @param {string} appId 
  * @returns 
  */
-const loginTo = async (appId) => {
+const loginTo = (appId) => {
     log("Logging in " + appId);
-    client = new rpc.Client({ transport: 'ipc' })
-    return client.login({ clientId: appId }).catch(() => {
+    // client = new rpc.Client({ transport: 'ipc' })
+    return client.login({ clientId: appId }).catch((error) => {
         log(`Failed to login with the applicationID '${appId}'.`);
+        console.error(error);
     })
 }
+
+
 
 /**
  * Logs into the Discord Application to change the game status
@@ -147,7 +153,7 @@ const loginTo = async (appId) => {
  * @returns 
  */
 const loginIfNeeded = async (appId) => {
-    if (appId == activeAppId) {
+    if (appId == activeAppId && activeAppId == defaultAppId) {
         return;
     }
     if (!appId) {
@@ -156,17 +162,16 @@ const loginIfNeeded = async (appId) => {
     if (appId) {
         activeAppId = appId;
     }
-    if (client) {
-        log("Destroying", activeAppId);
-        await client.destroy();
+    if (client && clientConnected) {
+        // log("Destroying " + activeAppId);
     }
-    client = new rpc.Client({ transport: 'ipc' })
     const activeAppLogin = await loginTo(activeAppId);
+
     if (!activeAppLogin) {
         activeAppId = defaultAppId;
-        await client.destroy();
         await loginTo(activeAppId);
     }
+    clientConnected = true;
 }
 
 /**
@@ -174,8 +179,9 @@ const loginIfNeeded = async (appId) => {
  */
 const update = async () => {
     const newIndex = Number.parseInt(Math.random() * statusContentList.length);
-    if (!forceChange)
-        log("The current run has no changes. Waiting for next run.");
+    // TOODO forceChange
+    // if (!forceChange)
+    //     log("The current run has no changes. Waiting for next run.");
     if (newIndex == currentIndex) return;
     currentIndex = newIndex;
     await loginIfNeeded(statusContentList[currentIndex].appId);
